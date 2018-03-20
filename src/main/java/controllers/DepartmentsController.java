f65ed170839c22f29c081d268193e81467ba7d38
Chris Murphy <_chrism_@live.com>
2018-03-20 19:29:38 +0000

    routes created to get a form for a new department and post result to departments view

--- src/main/java/controllers/DepartmentsController.java
+++ src/main/java/controllers/DepartmentsController.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class DepartmentsController {
 
@@ -23,6 +24,20 @@ public class DepartmentsController {
             model.put("departments", departments);
             model.put("template", "templates/departments/index.vtl");
             return new ModelAndView(model,"templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/departments/new", (req,res) ->{
+            HashMap<String,Object> model = new HashMap<>();
+            model.put("template","templates/departments/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        },new VelocityTemplateEngine());
+
+        post("/departments", (req,res) ->{
+            String title = req.queryParams("title");
+            Department department = new Department(title);
+            DBHelper.save(department);
+            res.redirect("/departments");
+            return null;
         }, new VelocityTemplateEngine());
     }
 }
