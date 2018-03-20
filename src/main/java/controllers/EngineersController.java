ec6a08daf0d341d8edf56a33071461194d1c7ce9
Chris Murphy <_chrism_@live.com>
2018-03-20 19:03:48 +0000

    routes created to get form for a new engineer and saves the new engineer to engineers page

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,14 +1,18 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class EngineersController {
     public EngineersController() {
@@ -22,6 +26,26 @@ public class EngineersController {
             model.put("engineers", engineers);
             model.put("template", "templates/engineers/index.vtl");
             return new ModelAndView(model,"templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/engineers/new", (req,res) ->{
+            HashMap<String,Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("template", "templates/engineers/create.vtl");
+            model.put("departments", departments);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/engineers", ( req, res) ->{
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId,Department.class);
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+            Engineer engineer = new Engineer(firstName,lastName,salary,department);
+            DBHelper.save(engineer);
+            res.redirect("/engineers");
+            return null;
         }, new VelocityTemplateEngine());
     }
 }
